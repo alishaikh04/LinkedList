c074aca19b3b6e974574094aada8a6cc16771177
alishaikh04 <alishaikhzk@gmail.com>
2026-04-01 11:25:33 +0500

    Update MultiLL.cpp

--- MultiLL.cpp
+++ MultiLL.cpp
@@ -1,457 +1,443 @@
-
 #include <iostream>
-#include <cstdlib>   // For malloc and free
-
 using namespace std;
 
-struct SNode {
-    int SNo;
+struct SNode
+{
+    int data;
     SNode* snext;
 };
 
-struct CNode {
-    int CNo;
+struct CNode
+{
+    int data;
     CNode* cnext;
     SNode* slist;
 };
 
 CNode* clist = NULL;
 
-/* ================= INSERT COURSE ================= */
-
-void Insertcourse(int CNo) {
-
-    CNode* temp = (CNode*)malloc(sizeof(CNode));
-
-    temp->CNo = CNo;
+void insertCourses(int courseNumber)
+{
+    CNode* temp = new CNode();
+    temp->data = courseNumber;
     temp->cnext = NULL;
     temp->slist = NULL;
 
-    if (clist == NULL) {
+    if (clist == NULL)
         clist = temp;
+    else
+    {
+        CNode* ccurrent = clist;
+        while (ccurrent->cnext != NULL)
+            ccurrent = ccurrent->cnext;
+        ccurrent->cnext = temp;
     }
-    else {
-        CNode* current = clist;
-
-        while (current->cnext != NULL) {
-            current = current->cnext;
-        }
-
-        current->cnext = temp;
-    }
+    cout << "Course " << courseNumber << " Inserted." << endl;
 }
 
-/* ================= ENROLL STUDENT ================= */
-
-void enrollingStudent(int CNo, int SNo) {
-
-    if (clist == NULL) {
-        cout << "Course list is empty." << endl;
+void enrollstudentincourse(int courseNumber, int studentNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
 
     CNode* ccurrent = clist;
+    bool found = false;
 
-    while (ccurrent != NULL) {
-
-        if (ccurrent->CNo == CNo) {
-
-            SNode* temp = (SNode*)malloc(sizeof(SNode));
-
-            temp->SNo = SNo;
+    while (ccurrent != NULL)
+    {
+        if (courseNumber == ccurrent->data)
+        {
+            found = true;
+            SNode* temp = new SNode();
+            temp->data = studentNumber;
             temp->snext = NULL;
 
-            if (ccurrent->slist == NULL) {
+            if (ccurrent->slist == NULL)
                 ccurrent->slist = temp;
-            }
-            else {
-
+            else
+            {
                 SNode* scurrent = ccurrent->slist;
-
-                while (scurrent->snext != NULL) {
+                while (scurrent->snext != NULL)
                     scurrent = scurrent->snext;
-                }
-
                 scurrent->snext = temp;
             }
-
-            cout << "Student enrolled successfully." << endl;
-            return;
+            cout << "Student " << studentNumber << " Enrolled in Course " << courseNumber << endl;
         }
-
         ccurrent = ccurrent->cnext;
     }
 
-    cout << "Course not found." << endl;
+    if (!found)
+        cout << "Course Not Found." << endl;
 }
 
-/* ================= SEARCH COURSE ================= */
-
-void searchingCourse(int CNo) {
-
-    if (clist == NULL) {
-        cout << "Course list is empty." << endl;
+void searchfortheCourse(int courseNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
 
     CNode* ccurrent = clist;
+    bool found = false;
+    int position = 0;
 
-    while (ccurrent != NULL) {
-
-        if (ccurrent->CNo == CNo) {
-            cout << "Course found." << endl;
+    while (ccurrent != NULL)
+    {
+        position++;
+        if (ccurrent->data == courseNumber)
+        {
+            cout << "Course " << courseNumber << " found at position " << position << endl;
+            found = true;
             return;
         }
-
         ccurrent = ccurrent->cnext;
     }
 
-    cout << "Course not found." << endl;
+    if (!found)
+        cout << "Course Not Found." << endl;
 }
 
-/* ================= SEARCH STUDENT IN COURSE ================= */
-
-void searchStudentInCourse(int CNo) {
-
-    if (clist == NULL) {
-        cout << "Course list is empty." << endl;
+void searchforstudentinstudentlist(int studentNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
 
     CNode* ccurrent = clist;
-
-    while (ccurrent != NULL) {
-
-        if (ccurrent->CNo == CNo) {
-
-            SNode* scurrent = ccurrent->slist;
-
-            if (scurrent == NULL) {
-                cout << "No students in this course." << endl;
-                return;
-            }
-
-            cout << "Students in course " << CNo << ": " << endl;
-
-            while (scurrent != NULL) {
-                cout << scurrent->SNo << " ";
-                scurrent = scurrent->snext;
-            }
-
-            cout << endl;
-            return;
-        }
-
-        ccurrent = ccurrent->cnext;
-    }
-
-    cout << "Course not found." << endl;
-}
-
-/* ================= STUDENT ENROLLED IN WHICH COURSE ================= */
-
-void studentEnrolledInwhichCourse(int SNo) {
-
-    if (clist == NULL) {
-        cout << "No course list." << endl;
-        return;
-    }
-
     bool found = false;
 
-    CNode* ccurrent = clist;
-
-    while (ccurrent != NULL) {
-
+    while (ccurrent != NULL)
+    {
         SNode* scurrent = ccurrent->slist;
-
-        while (scurrent != NULL) {
-
-            if (scurrent->SNo == SNo) {
-                cout << "Student enrolled in course: " << ccurrent->CNo << endl;
+        while (scurrent != NULL)
+        {
+            if (scurrent->data == studentNumber)
+            {
+                cout << "Student " << studentNumber << " found in Course " << ccurrent->data << endl;
                 found = true;
             }
-
             scurrent = scurrent->snext;
         }
-
         ccurrent = ccurrent->cnext;
     }
 
-    if (!found) {
-        cout << "Student not enrolled in any course." << endl;
-    }
+    if (!found)
+        cout << "Student Not Found." << endl;
 }
 
-/* ================= DELETE COURSE ================= */
-
-void deleteCourse(int CNo) {
-
-    if (clist == NULL) {
-        cout << "List is empty." << endl;
+void searchStudentinCourse(int courseNumber, int studentNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
-
-    CNode* current = clist;
-    CNode* previous = NULL;
-
-    while (current != NULL) {
-
-        if (current->CNo == CNo) {
-
-            if (previous == NULL) {
-                clist = current->cnext;
-            }
-            else {
-                previous->cnext = current->cnext;
-            }
-
-            // Delete students
-            SNode* scurrent = current->slist;
-
-            while (scurrent != NULL) {
-
-                SNode* temp = scurrent;
-                scurrent = scurrent->snext;
-                free(temp);
-            }
-
-            free(current);
-
-            cout << "Course deleted successfully." << endl;
-            return;
-        }
-
-        previous = current;
-        current = current->cnext;
-    }
-
-    cout << "Course not found." << endl;
-}
-
-/* ================= DELETE STUDENT FROM ALL COURSES ================= */
-
-void deleteStudent(int SNo) {
-
-    if (clist == NULL) {
-        cout << "No courses available." << endl;
-        return;
-    }
-
-    bool found = false;
 
     CNode* ccurrent = clist;
 
-    while (ccurrent != NULL) {
+    while (ccurrent != NULL)
+    {
+        if (ccurrent->data == courseNumber)
+        {
+            SNode* scurrent = ccurrent->slist;
+            while (scurrent != NULL)
+            {
+                if (scurrent->data == studentNumber)
+                {
+                    cout << "Student " << studentNumber << " found in Course " << courseNumber << endl;
+                    return;
+                }
+                scurrent = scurrent->snext;
+            }
+        }
+        ccurrent = ccurrent->cnext;
+    }
 
+    cout << "Course or Student Not Found." << endl;
+}
+
+void deleteCourse(int courseNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
+        return;
+    }
+
+    CNode* ccurrent = clist;
+
+    if (ccurrent->data == courseNumber)
+    {
+        clist = ccurrent->cnext;
+        delete ccurrent;
+        cout << "Course " << courseNumber << " Deleted." << endl;
+        return;
+    }
+
+    CNode* previous = ccurrent;
+    ccurrent = ccurrent->cnext;
+
+    while (ccurrent != NULL)
+    {
+        if (ccurrent->data == courseNumber)
+        {
+            previous->cnext = ccurrent->cnext;
+            delete ccurrent;
+            cout << "Course " << courseNumber << " Deleted." << endl;
+            return;
+        }
+        previous = ccurrent;
+        ccurrent = ccurrent->cnext;
+    }
+
+    cout << "Course Not Found." << endl;
+}
+
+void deleteStudent(int studentNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
+        return;
+    }
+
+    CNode* ccurrent = clist;
+    bool found = false;
+
+    while (ccurrent != NULL)
+    {
         SNode* scurrent = ccurrent->slist;
         SNode* previous = NULL;
 
-        while (scurrent != NULL) {
-
-            if (scurrent->SNo == SNo) {
-
-                if (previous == NULL) {
+        while (scurrent != NULL)
+        {
+            if (scurrent->data == studentNumber)
+            {
+                if (previous == NULL)
                     ccurrent->slist = scurrent->snext;
-                }
-                else {
+                else
                     previous->snext = scurrent->snext;
-                }
 
-                SNode* temp = scurrent;
-                scurrent = scurrent->snext;
-
-                free(temp);
-
-                cout << "Student removed from course " << ccurrent->CNo << endl;
-
+                delete scurrent;
                 found = true;
-                continue;
+                cout << "Student " << studentNumber << " deleted from Course " << ccurrent->data << endl;
+                break;
             }
-
             previous = scurrent;
             scurrent = scurrent->snext;
         }
-
         ccurrent = ccurrent->cnext;
     }
 
-    if (!found) {
-        cout << "Student not found." << endl;
-    }
+    if (!found)
+        cout << "Student Not Found." << endl;
 }
 
-/* ================= DELETE STUDENT FROM SPECIFIC COURSE ================= */
-
-void deleteStudentFromCourse(int CNo, int SNo) {
-
-    if (clist == NULL) {
-        cout << "Course list empty." << endl;
+void deleteStudentInTheCourse(int courseNumber, int studentNumber)
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
 
     CNode* ccurrent = clist;
 
-    while (ccurrent != NULL) {
-
-        if (ccurrent->CNo == CNo) {
-
-            SNode* scurrent = ccurrent->slist;
+    while (ccurrent != NULL)
+    {
+        if (ccurrent->data == courseNumber)
+        {
             SNode* previous = NULL;
+            SNode* scurrent = ccurrent->slist;
 
-            while (scurrent != NULL) {
-
-                if (scurrent->SNo == SNo) {
-
-                    if (previous == NULL) {
+            while (scurrent != NULL)
+            {
+                if (scurrent->data == studentNumber)
+                {
+                    if (previous == NULL)
                         ccurrent->slist = scurrent->snext;
-                    }
-                    else {
+                    else
                         previous->snext = scurrent->snext;
-                    }
 
-                    free(scurrent);
-
-                    cout << "Student removed from course." << endl;
+                    delete scurrent;
+                    cout << "Student " << studentNumber << " Deleted From Course " << courseNumber << endl;
                     return;
                 }
-
                 previous = scurrent;
                 scurrent = scurrent->snext;
             }
-
-            cout << "Student not found in this course." << endl;
-            return;
         }
-
         ccurrent = ccurrent->cnext;
     }
 
-    cout << "Course not found." << endl;
+    cout << "Course or Student Not Found." << endl;
 }
 
-/* ================= DISPLAY COURSES ================= */
-
-void displayCourse() {
-
-    if (clist == NULL) {
-        cout << "Course list empty." << endl;
+void displayCourses()
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
-
-    CNode* current = clist;
-
-    cout << "Courses:" << endl;
-
-    while (current != NULL) {
-        cout << current->CNo << " ";
-        current = current->cnext;
-    }
-
-    cout << endl;
-}
-
-/* ================= DISPLAY ALL ================= */
-
-void displayAll() {
 
     CNode* ccurrent = clist;
+    while (ccurrent != NULL)
+    {
+        cout << ccurrent->data << " -> ";
+        ccurrent = ccurrent->cnext;
+    }
+    cout << "NULL" << endl;
+}
 
-    if (ccurrent == NULL) {
-        cout << "No data." << endl;
+void displaystudents()
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
         return;
     }
 
-    while (ccurrent != NULL) {
-
-        cout << "Course " << ccurrent->CNo << " -> ";
-
+    CNode* ccurrent = clist;
+    while (ccurrent != NULL)
+    {
+        cout << "Course " << ccurrent->data << " -> Students: ";
         SNode* scurrent = ccurrent->slist;
-
-        while (scurrent != NULL) {
-            cout << scurrent->SNo << " ";
+        while (scurrent != NULL)
+        {
+            cout << scurrent->data << " -> ";
             scurrent = scurrent->snext;
         }
-
-        cout << endl;
-
+        cout << "NULL" << endl;
         ccurrent = ccurrent->cnext;
     }
 }
 
-/* ================= MAIN ================= */
+void displayCoursesandstudents()
+{
+    if (clist == NULL)
+    {
+        cout << "Course List is Empty." << endl;
+        return;
+    }
 
-int main() {
-
-    int cnumber, scode, num;
-
-    while (true) {
-
-        cout << "\n1. Insert Course." << endl;
-        cout << "2. Enroll Student." << endl;
-        cout << "3. Search Course." << endl;
-        cout << "4. Search Students in Course." << endl;
-        cout << "5. Student Courses." << endl;
-        cout << "6. Delete Course." << endl;
-        cout << "7. Delete Student (All Courses)." << endl;
-        cout << "8. Delete Student From Course." << endl;
-        cout << "9. Display Courses." << endl;
-        cout << "10. Display Course Students." << endl;
-        cout << "11. Display All." << endl;
-        cout << "12. Exit." << endl;
-
-        cin >> num;
-
-        if (num == 1) {
-            cin >> cnumber;
-            Insertcourse(cnumber);
+    CNode* ccurrent = clist;
+    while (ccurrent != NULL)
+    {
+        cout << "Course: " << ccurrent->data << endl;
+        SNode* scurrent = ccurrent->slist;
+        while (scurrent != NULL)
+        {
+            cout << scurrent->data << " -> ";
+            scurrent = scurrent->snext;
         }
+        cout << "NULL" << endl;
+        ccurrent = ccurrent->cnext;
+    }
+}
 
-        else if (num == 2) {
-            cin >> cnumber >> scode;
-            enrollingStudent(cnumber, scode);
+int main()
+{
+    int choice, studentNumber, courseNumber;
+
+    while (true)
+    {
+        cout << endl;
+        cout << "1.  Insert Course."               << endl;
+        cout << "2.  Enroll Student in Course."    << endl;
+        cout << "3.  Search Course."               << endl;
+        cout << "4.  Search Student."              << endl;
+        cout << "5.  Search Student in Course."    << endl;
+        cout << "6.  Delete Course."               << endl;
+        cout << "7.  Delete Student."              << endl;
+        cout << "8.  Delete Student in Course."    << endl;
+        cout << "9.  Display Courses and Students."<< endl;
+        cout << "10. Display Courses Only."        << endl;
+        cout << "11. Display Students Only."       << endl;
+        cout << "12. Exit."                        << endl << endl;
+        cout << "Enter Choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            cout << "Enter Course Number: ";
+            cin >> courseNumber;
+            insertCourses(courseNumber);
         }
-
-        else if (num == 3) {
-            cin >> cnumber;
-            searchingCourse(cnumber);
+        else if (choice == 2)
+        {
+            cout << "Enter Course Number: ";
+            cin >> courseNumber;
+            cout << "Enter Student Number: ";
+            cin >> studentNumber;
+            enrollstudentincourse(courseNumber, studentNumber);
         }
-
-        else if (num == 4) {
-            cin >> cnumber;
-            searchStudentInCourse(cnumber);
+        else if (choice == 3)
+        {
+            cout << "Enter Course Number: ";
+            cin >> courseNumber;
+            searchfortheCourse(courseNumber);
         }
-
-        else if (num == 5) {
-            cin >> scode;
-            studentEnrolledInwhichCourse(scode);
+        else if (choice == 4)
+        {
+            cout << "Enter Student Number: ";
+            cin >> studentNumber;
+            searchforstudentinstudentlist(studentNumber);
         }
-
-        else if (num == 6) {
-            cin >> cnumber;
-            deleteCourse(cnumber);
+        else if (choice == 5)
+        {
+            cout << "Enter Course Number: ";
+            cin >> courseNumber;
+            cout << "Enter Student Number: ";
+            cin >> studentNumber;
+            searchStudentinCourse(courseNumber, studentNumber);
         }
-
-        else if (num == 7) {
-            cin >> scode;
-            deleteStudent(scode);
+        else if (choice == 6)
+        {
+            cout << "Enter Course Number: ";
+            cin >> courseNumber;
+            deleteCourse(courseNumber);
         }
-
-        else if (num == 8) {
-            cin >> cnumber >> scode;
-            deleteStudentFromCourse(cnumber, scode);
+        else if (choice == 7)
+        {
+            cout << "Enter Student Number: ";
+            cin >> studentNumber;
+            deleteStudent(studentNumber);
         }
-
-        else if (num == 9) {
-            displayCourse();
+        else if (choice == 8)
+        {
+            cout << "Enter Course Number: ";
+            cin >> courseNumber;
+            cout << "Enter Student Number: ";
+            cin >> studentNumber;
+            deleteStudentInTheCourse(courseNumber, studentNumber);
         }
-
-        else if (num == 11) {
-            displayAll();
+        else if (choice == 9)
+        {
+            displayCoursesandstudents();
         }
-
-        else if (num == 12) {
+        else if (choice == 10)
+        {
+            displayCourses();
+        }
+        else if (choice == 11)
+        {
+            displaystudents();
+        }
+        else if (choice == 12)
+        {
+            cout << "Exiting Program." << endl;
             break;
+        }
+        else
+        {
+            cout << "Invalid Choice." << endl;
         }
     }
 
